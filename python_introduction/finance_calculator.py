1c7d21a7f8f2c7bf08a2c35ba5efc0bbde4172ce
Wonder Nxumalo <144373619+WonderNxumalo@users.noreply.github.com>
2025-09-08 17:40:31 +0200

    Added finance calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -10,5 +10,4 @@ print(f"""
       Your monthly savings are $ {monthly_savings}.
       Projected savings after one year, with interest, is: $ {projected_savings}.
       """)
-
-""" /tmp/correction/7446214723889332675741141749742951659150_5/100739/921620/python_introduction/finance_calculator.py doesn't contain monthly_savings\s*=\s*(monthly_income\s*-\s*monthly_expenses|float\s*\(\s*monthly_income\s*\)\s*-\s*float\s*\(\s*monthly_expenses\s*\)) """
+monthly_income = float(monthly_income) - float(monthly_expenses)
